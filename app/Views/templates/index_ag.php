a3527bf0813e2ee195c188bb2c1f6271e3b0b3a6
AlkhadafiS <alkhadafi.saddam21@students.unila.ac.id>
2023-11-27 08:57:01 +0700

    benerin filter part 2

--- app/Views/templates/index_ag.php
+++ app/Views/templates/index_ag.php
@@ -50,7 +50,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; indoapril <?= date('Y'); ?></span>
+                        <span>Copyright &copy; Indoapril <?= date('Y'); ?></span>
                     </div>
                 </div>
             </footer>
